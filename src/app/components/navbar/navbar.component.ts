d9c92b0323c72656196329afe914bd8881313a8b
Ospino <ospino.p@gmail.com>
2020-05-11 16:44:45 +0200

    change navbar to bulma
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  is_active = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  collapse() {
+    if (!this.is_active) {
+      document.getElementById("navbar-burger").classList.add("is-active");
+      document.getElementById("navbar-menu").classList.add("is-active");
+      console.log("HELO");
+      this.is_active = true;
+    } else {
+      document.getElementById("navbar-burger").classList.remove("is-active");
+      document.getElementById("navbar-menu").classList.remove("is-active");
+      this.is_active = false;
+    }
+  }
+
+  showModalSignUp() {
+    document.getElementById("sign_up_modal").classList.add("is-active");
+    document.getElementById("sign_up_modal").classList.add("is-clipped");
+  }
+
+  closeModalSignUp() {
+    document.getElementById("sign_up_modal").classList.remove("is-active");
+    document.getElementById("sign_up_modal").classList.remove("is-clipped");
+  }
+
+  showModalSignIn() {
+    document.getElementById("sign_in_modal").classList.add("is-active");
+    document.getElementById("sign_in_modal").classList.add("is-clipped");
+
+  }
+
+  closeModalSignIn() {
+    document.getElementById("sign_in_modal").classList.remove("is-active");
+    document.getElementById("sign_in_modal").classList.remove("is-clipped");
+  }
+
+  signUp() {
+    console.log("signUp");
+  }
+
+  signIn() {
+    console.log("signIn");
+  }
 }
