abd5787483782ed84b0529e1e11727e80966ad9c
Ospino <ospino.p@gmail.com>
2020-05-11 16:46:43 +0200

    add routing
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventDetailsComponent } from './components/event-details/event-details.component';
+import { EventsListComponent } from './components/events-list/events-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EventsListComponent },
+  { path: "events/:id", component: EventDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
