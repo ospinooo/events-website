22942e774cd65d38a56df7623c0c91e7ebeba77b
Ospino <ospino.p@gmail.com>
2020-05-11 16:45:16 +0200

    change list to bulma
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/components/events-list/events-list.component.ts
+++ src/app/components/events-list/events-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from '../event-details/event.model';
 
 @Component({
   selector: 'app-events-list',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsListComponent implements OnInit {
 
-  constructor() { }
+  events = [
+    new Event("Event 1 ", 1),
+    new Event("Event 2 ", 2),
+    new Event("Event 3 ", 3),
+    new Event("Event 4 ", 4),
+    new Event("Event 5 ", 5),
+    new Event("Event 6 ", 6),
+    new Event("Event 7 ", 7),
+    new Event("Event 8 ", 8),
+    new Event("Event 9 ", 9),
+    new Event("Event 10 ", 10),
+  ]
+
+
+  /**
+   * Each time we construct the events list we are going to pass in params a Search key
+   */
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+
+  }
+
+  range(end): Array<Number> {
+    var ans = [];
+    for (let i = 0; i <= end; i++) {
+      ans.push(i);
+    }
+    return ans;
   }
 
 }
