a5a02ab8f88b5a6ecc076325d1b0b0f41477af27
Ospino <ospino.p@gmail.com>
2020-05-11 16:46:57 +0200

    import the new components to the app module
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { EventsListComponent } from './components/events-list/events-list.component';
 import { LateralNavbarComponent } from './components/lateral-navbar/lateral-navbar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { EventCardComponent } from './components/event-card/event-card.component';
+import { EventDetailsComponent } from './components/event-details/event-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsListComponent,
     LateralNavbarComponent,
-    NavbarComponent
+    NavbarComponent,
+    EventCardComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule,
