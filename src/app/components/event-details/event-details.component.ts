9e15239913c50b1bcdeba77f8d00d44edc2bbe70
Ospino <ospino.p@gmail.com>
2020-05-12 10:05:55 +0200

    add more info and styling in the event-details
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/components/event-details/event-details.component.ts
+++ src/app/components/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventsService } from 'src/app/services/events.service';
+import { Event } from './event.model';
 
 @Component({
   selector: 'app-event-details',
@@ -10,37 +11,49 @@ import { EventsService } from 'src/app/services/events.service';
 export class EventDetailsComponent implements OnInit {
 
   id: number;
-  private sub: any;
+  event_model: Event;
+  sub;
+  fees = [
+    { 'name': "fee1" },
+    { 'name': "fee2" },
+    { 'name': "fee3" }
+  ];
 
-  firstname: string;
-  lastname: string;
-  email: string;
-  telephone: string;
-
-
-  constructor(private route: ActivatedRoute, private contactService: EventsService) {
+  constructor(private route: ActivatedRoute, private eventsService: EventsService) {
 
   }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.id = +params['id']; // (+) converts string 'id' to a number
-      // this.getContact()
+      this.event_model = this.eventsService.getEvent(this.id);
     })
   }
 
-  getContact() {
-    // this.contactService.getContact(this.id).subscribe((contact) => {
+  getEvent() {
+    // this.eventsService.getEvent(this.id).subscribe((contact) => {
     //   this.id = contact.id;
     //   this.firstname = contact.firstname;
     //   this.lastname = contact.lastname;
     //   this.email = contact.email;
     //   this.telephone = contact.telephone;
     // })
+
+    this.event_model = this.eventsService.getEvent(this.id);
   }
 
 
   ngOnDestroy() {
     // this.sub.unsubscribe();
+  }
+
+  openBuyTicketsModal() {
+    document.getElementById("buy_tickets_modal").classList.add("is-active");
+    document.getElementById("buy_tickets_modal").classList.add("is-clipped");
+  }
+
+  closeBuyTicketsModal() {
+    document.getElementById("buy_tickets_modal").classList.remove("is-active");
+    document.getElementById("buy_tickets_modal").classList.remove("is-clipped");
   }
 }
--- src/app/components/event-details/event.model.ts
+++ src/app/components/event-details/event.model.ts
@@ -1,9 +1,11 @@
 export class Event {
   id: number;
   title: string;
+  description: string;
 
-  constructor(title: string, id?: number) {
+  constructor(title: string, description: string, id: number) {
     this.id = id;
     this.title = title;
+    this.description = description;
   }
 }
