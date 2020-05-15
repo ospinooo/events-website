295ab9cc3e6b7a65fd2ba0e29fe36d0f90c65ce3
Ospino <ospino.p@gmail.com>
2020-05-15 12:21:20 +0200

    refactor imports from event model
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/components/payment-modal/payment-modal.component.ts
+++ src/app/components/payment-modal/payment-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Event } from "../event-details/event.model";
+import { Event } from "../../models/event.model";
 
 @Component({
   selector: 'app-payment-modal',
