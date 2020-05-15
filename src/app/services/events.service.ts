226b82e10c0f0497807fded6ee1e2a7b4757fc83
Ospino <ospino.p@gmail.com>
2020-05-15 12:14:45 +0200

    implement Get collection and one events
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,33 +1,79 @@
 import { Injectable } from '@angular/core';
 import { Event } from '../components/event-details/event.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { PageableEvent } from './res/events.interface';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
+
+
+export class Pageable {
+
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  events = [
-    new Event("Event 1 ", "Event 1 DESCRIPTION", 1),
-    new Event("Event 2 ", "Event 2 DESCRIPTION", 2),
-    new Event("Event 3 ", "Event 3 DESCRIPTION", 3),
-    new Event("Event 4 ", "Event 4 DESCRIPTION", 4),
-    new Event("Event 5 ", "Event 5 DESCRIPTION", 5),
-    new Event("Event 6 ", "Event 6 DESCRIPTION", 6),
-    new Event("Event 7 ", "Event 7 DESCRIPTION", 7),
-    new Event("Event 8 ", "Event 8 DESCRIPTION", 8),
-    new Event("Event 9 ", "Event 9 DESCRIPTION", 9),
-    new Event("Event 10 ", "Event 10 DESCRIPTION", 10),
-  ]
+  constructor(private http: HttpClient) { }
+
+  private eventsUrl = 'http://localhost:8080/events';
 
 
-  constructor() {
+  /** GET events from the server */
+  getEvents(): Observable<PageableEvent> {
+    return this.http.get<PageableEvent>(this.eventsUrl);
   }
 
-  getEvents(): Array<Event> {
-    return this.events;
+  /** GET contact by id. Will 404 if id not found */
+  getEvent(id: number): Observable<Event> {
+    const url = `${this.eventsUrl}/${id}`;
+    return this.http.get<Event>(url, httpOptions).pipe(
+      tap(_ => this.log(`fetched event id=${id}`)),
+      catchError(this.handleError<Event>(`getEvent id=${id}`))
+    );
   }
 
-  getEvent(id: number): Event {
-    return this.events[id];
+  /** DELETE: delete the contact from the server */
+  deleteEvent(event: Event | number): Observable<Event> {
+    const id = typeof event === 'number' ? event : event.id;
+    const url = `${this.eventsUrl}/${id}`;
+    return this.http.delete<Event>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted event id=${id}`)),
+      catchError(this.handleError<Event>('deleteEvent'))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a ContactService message with the MessageService */
+  private log(message: string) {
+    console.log('EventService: ' + message);
   }
 }
