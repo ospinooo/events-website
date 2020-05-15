0b40ec8f7186e7c51d0af5ec4ce9f4c0cd9908fa
Ospino <ospino.p@gmail.com>
2020-05-15 13:50:35 +0200

    add authorization service with the attempt auth and the signUp
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/auth/authentication.service.ts
+++ src/app/auth/authentication.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'COntent-Type': 'application/json' })
+};
+
+export class JwtResponse {
+  token: string;
+  type: string;
+  username: string;
+  authorities: string[];
+}
+
+
+export class LoginInfo {
+  username: string;
+  password: string;
+
+  constructor(username: string, password: string) {
+    this.username = username;
+    this.password = password;
+  }
+}
+
+export class SignupInfo {
+
+  username: string;
+  role: string[];
+  password: string;
+
+  constructor(username: string, password: string) {
+    this.username = username;
+    this.role = ['user'];
+    this.password = password;
+  }
+}
+
+
+export enum Role {
+  ADMIN = "ROLE_ADMIN",
+  USER = "ROLE_USER"
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private loginUrl = 'http://localhost:8080/auth/signin';
+  private signupUrl = 'http://localhost:8080/auth/signup';
+
+
+  constructor(private http: HttpClient) { }
+
+  attemptAuth(credentials: LoginInfo): Observable<JwtResponse> {
+    return this.http.post<JwtResponse>(this.loginUrl, credentials, httpOptions);
+  }
+
+  signUp(info: SignupInfo): Observable<string> {
+    return this.http.post<string>(this.signupUrl, info, httpOptions);
+  }
 }
