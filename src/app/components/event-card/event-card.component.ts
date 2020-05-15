dd23415790e83e56d84449570f634a06a83306a6
Ospino <ospino.p@gmail.com>
2020-05-15 12:21:04 +0200

    refactor imports from event model
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/components/event-card/event-card.component.ts
+++ src/app/components/event-card/event-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Event } from '../event-details/event.model';
+import { Event } from '../../models/event.model';
 
 @Component({
   selector: 'app-event-card',
