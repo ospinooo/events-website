d5056eed5b998a5285f05257f979b0cf77244e3d
Ospino <ospino.p@gmail.com>
2020-05-15 12:20:49 +0200

    refactor imports from event model
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Event } from '../components/event-details/event.model';
+import { Event } from '../models/event.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
