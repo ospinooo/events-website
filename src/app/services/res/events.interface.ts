1dd59dbf363d302483d8433a4ce2fef600548598
Ospino <ospino.p@gmail.com>
2020-05-15 12:13:51 +0200

    add eventsPageable interface to define the type of the data returned by getEvents
    Signed-off-by: Ospino <ospino.p@gmail.com>

---
+++ src/app/services/res/events.interface.ts
@@ -0,0 +1,45 @@
+import { Event } from '../../components/event-details/event.model';
+
+export interface Fee {
+  id: number;
+  price: number;
+  title: string;
+  description: string;
+  eventId: number;
+}
+
+
+export interface Sort {
+  sorted: boolean;
+  unsorted: boolean;
+  empty: boolean;
+}
+
+export interface Pageable {
+  sort: Sort;
+  offset: number;
+  pageNumber: number;
+  pageSize: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+export interface Sort2 {
+  sorted: boolean;
+  unsorted: boolean;
+  empty: boolean;
+}
+
+export interface PageableEvent {
+  content: Event[];
+  pageable: Pageable;
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+  size: number;
+  number: number;
+  first: boolean;
+  numberOfElements: number;
+  sort: Sort2;
+  empty: boolean;
+}
