9003e054406248d13fbc98cf0481cef203f4efed
Ospino <ospino.p@gmail.com>
2020-05-18 11:21:40 +0200

    use the isAdmin and isLogged from the tokenStorage in the laterla navbar to show items or not
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/components/lateral-navbar/lateral-navbar.component.ts
+++ src/app/components/lateral-navbar/lateral-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from 'src/app/auth/token-storage.service';
 
 @Component({
   selector: 'app-lateral-navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LateralNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public tokenStorageService: TokenStorageService) { }
 
   ngOnInit(): void {
   }
