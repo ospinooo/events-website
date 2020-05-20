698fe980e73b3124c384ded311b09f60f500efc7
Ospino <ospino.p@gmail.com>
2020-05-20 02:41:15 +0200

    lateral navbar active tab is active class
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/components/lateral-navbar/lateral-navbar.component.ts
+++ src/app/components/lateral-navbar/lateral-navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from 'src/app/auth/token-storage.service';
 
+
+
 @Component({
   selector: 'app-lateral-navbar',
   templateUrl: './lateral-navbar.component.html',
@@ -8,9 +10,14 @@ import { TokenStorageService } from 'src/app/auth/token-storage.service';
 })
 export class LateralNavbarComponent implements OnInit {
 
+  active: string;
+
   constructor(public tokenStorageService: TokenStorageService) { }
 
   ngOnInit(): void {
   }
 
+  newActive(active: string) {
+    this.active = active;
+  }
 }
