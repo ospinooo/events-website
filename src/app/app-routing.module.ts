5f9684f656a3deecb19284b407016b90db1e2bc3
Ospino <ospino.p@gmail.com>
2020-05-20 02:47:43 +0200

    add new components to the navbar and router
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EventDetailsComponent } from './components/event-details/event-details.component';
+import { EventDetailsComponent } from './components/events/event-details/event-details.component';
 import { EventsListComponent } from './components/events-list/events-list.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { CreateEventComponent } from './components/events/create-event/create-event.component';
 
 
 const routes: Routes = [
@@ -12,7 +13,9 @@ const routes: Routes = [
   { path: "events/:id", component: EventDetailsComponent },
   { path: "about", component: AboutComponent },
   { path: "profile", component: ProfileComponent },
-  { path: 'admin', component: AdminComponent }
+  { path: 'admin', component: AdminComponent },
+  { path: 'event/create', component: CreateEventComponent },
+  { path: 'event/edit', component: CreateEventComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import { EventsListComponent } from './components/events-list/events-list.component';
 import { LateralNavbarComponent } from './components/lateral-navbar/lateral-navbar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { EventCardComponent } from './components/event-card/event-card.component';
-import { EventDetailsComponent } from './components/event-details/event-details.component';
+import { EventCardComponent } from './components/events/event-card/event-card.component';
+import { EventDetailsComponent } from './components/events/event-details/event-details.component';
 import { PaymentModalComponent } from './components/payment-modal/payment-modal.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProfileComponent } from './pages/profile/profile.component';
@@ -17,6 +17,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { httpInterceptorProviders } from './auth/auth.interceptor';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CreateEventComponent } from './components/events/create-event/create-event.component';
+import { EditEventComponent } from './components/events/edit-event/edit-event.component';
 
 
 @NgModule({
@@ -32,7 +34,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ProfileComponent,
     AdminComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    CreateEventComponent,
+    EditEventComponent
   ],
   imports: [
     BrowserModule,
