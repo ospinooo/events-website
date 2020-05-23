6064f00c974121b8c57caf1c43f07fe62d37f272
Ospino <ospino.p@gmail.com>
2020-05-23 19:26:41 +0200

    add constructor and setFees to class Event
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/models/event.model.ts
+++ src/app/models/event.model.ts
@@ -9,9 +9,16 @@ export class Event {
   fees: Fee[];
   userId: number;
 
-  constructor(title: string, description: string, id: number) {
+  constructor(title: string, subtitle: string, description: string, id?: number, userId?: number, fees?: Fee[]) {
     this.id = id;
+    this.subtitle = subtitle;
     this.title = title;
     this.description = description;
+    this.fees = fees;
+    this.userId = userId;
+  }
+
+  setFees(fees: Fee[]) {
+    this.fees = fees;
   }
 }
