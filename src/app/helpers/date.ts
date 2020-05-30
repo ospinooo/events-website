df77db422a1ef22226c8578c2511b499eb56f305
Ospino <ospino.p@gmail.com>
2020-05-30 20:32:40 +0200

    add function getTodayString
    Signed-off-by: Ospino <ospino.p@gmail.com>

---
+++ src/app/helpers/date.ts
@@ -0,0 +1,10 @@
+
+
+export function getTodayString(): string {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
+
+  return `${yyyy}-${mm}-${dd}`;
+}
