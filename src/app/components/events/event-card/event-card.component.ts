d981fcf7c422bb707718d9a1d7db0c4ff8a7bbce
Ospino <ospino.p@gmail.com>
2020-06-09 16:00:57 +0200

    add past event difference background color
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/components/events/event-card/event-card.component.ts
+++ src/app/components/events/event-card/event-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Event, getCheapestPrice, getHighestPrice } from '../../../models/event.model';
+import { getTodayString } from 'src/app/helpers/date';
 
 @Component({
   selector: 'app-event-card',
@@ -9,10 +10,12 @@ import { Event, getCheapestPrice, getHighestPrice } from '../../../models/event.
 export class EventCardComponent implements OnInit {
 
   @Input() event_model: Event = undefined;
-
+  pastEvent: boolean;
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.pastEvent = new Date(this.event_model.date) < new Date(getTodayString());
+  }
 
   getEventCheapestPrice() {
     return getCheapestPrice(this.event_model);
