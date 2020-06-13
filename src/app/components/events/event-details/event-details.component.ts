89ae408f89c1ec760b10e16f8216befd00e6fcb7
Ospino <ospino.p@gmail.com>
2020-06-13 22:02:15 +0200

    disable buy buttom to past events
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/app/components/events/event-details/event-details.component.ts
+++ src/app/components/events/event-details/event-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventsService } from 'src/app/services/events.service';
 import { Event, getCheapestPrice } from '../../../models/event.model';
+import { getTodayString } from 'src/app/helpers/date';
 
 @Component({
   selector: 'app-event-details',
@@ -14,15 +15,18 @@ export class EventDetailsComponent implements OnInit {
 
   id: number;
   sub;
+  pastEvent: boolean;
 
   constructor(private route: ActivatedRoute, private eventsService: EventsService) {
 
   }
 
   ngOnInit() {
+
     this.sub = this.route.params.subscribe(params => {
       this.id = +params['id']; // (+) converts string 'id' to a number
       this.getEvent();
+      this.pastEvent = new Date(this.event_model.date) < new Date(getTodayString());
     })
   }
 
@@ -50,4 +54,6 @@ export class EventDetailsComponent implements OnInit {
   log(s) {
     console.log(s);
   }
+
+
 }
