fb89dc481b7f936c80a5de287a2e5fd0b113a226
Ospino <pablo.ospino@nispera.com>
2021-05-24 11:04:13 +0200

    https:
    Signed-off-by: Ospino <pablo.ospino@nispera.com>

--- src/app/services/http/http.options.ts
+++ src/app/services/http/http.options.ts
@@ -3,6 +3,6 @@ import { HttpHeaders } from '@angular/common/http';
 export const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': 'https://pabloospino.com',
   })
 };
